c52d57e9e845c4a64b9199b951d8a0ac57e60519
Sahra Slet <s8saslet@stud.uni-saarland.de>
2026-01-12 16:10:10 +0100

    modify k-means clustering script

--- src/audiolm/audio_tokenization.py
+++ src/audiolm/audio_tokenization.py
@@ -1,22 +1,41 @@
 """
-Audio tokenization using a pretrained encodec model.
+Audio tokenization using k-means clustering.
 Extracts discrete audio tokens.
 """
 
-from datasets import load_dataset, Audio
-from transformers import EncodecModel, AutoProcessor
-librispeech = load_dataset("hf-internal-testing/librispeech_asr", "clean", split="validation")
+import argparse
+import numpy as np
+from sklearn.cluster import KMeans
+from datasets import load_from_disk
 
-model = EncodecModel.from_pretrained("facebook/encodec_24khz")
-processor = AutoProcessor.from_pretrained("facebook/encodec_24khz")
-librispeech = librispeech.cast_column("audio", Audio(sampling_rate=processor.sampling_rate))
-audio_sample = librispeech[-1]["audio"]["array"]
-inputs = processor(raw_audio=audio_sample, sampling_rate=processor.sampling_rate, return_tensors="pt")
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_name", type=str, default="facebook/w2v-bert-2.0")
+parser.add_argument("--dataset_path", type=str, default="hf-internal-testing/librispeech_asr")
+parser.add_argument("--num_clusters", type=int, default=1024)
+args = parser.parse_args()
+def discretize_audio(args: argparse.Namespace):
+    '''Discretize audio into discrete tokens using K-Means.'''
 
-encoder_outputs = model.encode(inputs["input_values"], inputs["padding_mask"])
-# `encoder_outputs.audio_codes` contains discrete codes
-audio_values = model.decode(**encoder_outputs, padding_mask=inputs["padding_mask"])[0]
-# or the equivalent with a forward pass
-audio_values = model(inputs["input_values"], inputs["padding_mask"]).audio_values
+    dataset = load_from_disk(args.dataset_path)
+    feature_vectors = dataset["input_features"] #(seq_len, hidden_dim)
+
+    # We need shape (n_frames, hidden_dim) for K-Means, so stack all feature vectors
+    stacked_features = np.concatenate(feature_vectors, axis=0) #(total_frames, hidden_dim)
+
+    # K-Means clustering on all features
+    kmeans = KMeans(n_clusters=args.num_clusters, random_state=42)
+    kmeans.fit(stacked_features)
+
+    def assign_clusters(batch):
+        '''Assign cluster labels to each feature vector/frame in the batch.'''
+        batch["cluster"] = kmeans.predict(batch["input_features"])
+        return batch
+
+    # Assign clusters to each feature vector in the dataset
+    dataset = dataset.map(assign_clusters, batched=True)
+
+    return dataset
+
+
 
 
